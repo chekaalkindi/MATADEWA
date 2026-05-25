5d02a86c56d1d26f1f1c3014a71fbcfe2220dde0
chekaalkindi <75225490+chekaalkindi@users.noreply.github.com>
2026-05-25 11:23:35 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import argparse
+
+from modules.github_search import github_search
+from modules.reddit_search import reddit_search
+from modules.analyzer import analyze_results
+
+def banner():
+
+    print(r"""
+
+███╗   ███╗ █████╗ ████████╗ █████╗ ██████╗ ███████╗██╗    ██╗ █████╗
+████╗ ████║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║    ██║██╔══██╗
+██╔████╔██║███████║   ██║   ███████║██║  ██║█████╗  ██║ █╗ ██║███████║
+██║╚██╔╝██║██╔══██║   ██║   ██╔══██║██║  ██║██╔══╝  ██║███╗██║██╔══██║
+██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║██████╔╝███████╗╚███╔███╔╝██║  ██║
+╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═════╝ ╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝
+
+        MATADEWA OSINT CLI by chekaalkindi
+""")
+
+def main():
+
+    banner()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-q",
+        "--query",
+        required=True,
+        help="Target username / email / name"
+    )
+
+    args = parser.parse_args()
+
+    query = args.query
+
+    print(f"[+] Searching target: {query}\n")
+
+    findings = []
+
+    github_results = github_search(query)
+    reddit_results = reddit_search(query)
+
+    findings.extend(github_results)
+    findings.extend(reddit_results)
+
+    print("[+] Findings:\n")
+
+    for item in findings:
+        print(item)
+
+    print("\n[+] AI Analysis:\n")
+
+    summary = analyze_results(query, findings)
+
+    print(summary)
+
+if __name__ == "__main__":
+    main()
